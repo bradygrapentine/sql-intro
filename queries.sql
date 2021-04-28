0ab3009f329e5a79613f6636f385369e30bd94c7
bradygrapentine <brady.grapentine@gmail.com>
2021-04-28 14:38:52 -0400

    Done with explorer mode

--- queries.sql
+++ queries.sql
@@ -150,11 +150,141 @@ SELECT * FROM "Employees";
 --
 
 
--- EPIC MODE --
+-- 4/28/2021
+--
 CREATE TABLE "Departments"(
-  "Name"  TEXT,
-  "BuildingNumber"  INT NOT NULL
+  "DepartmentName"  TEXT,
+  "Building"  TEXT NOT NULL,
+  "Id"  SERIAL PRIMARY KEY 
 );
 -- CREATE TABLE
 --
 --
+ALTER TABLE "Employees" ADD COLUMN "DepartmentId" INTEGER NULL REFERENCES "Departments"("Id");
+-- You're about to run a destructive command.
+-- Do you want to proceed? (y/n): y
+-- Your call!
+-- ALTER TABLE
+--
+--
+CREATE TABLE "Products"(
+  "Price"  FLOAT,
+  "Name"  TEXT NOT NULL,
+  "Description" TEXT,
+  "QuantityInStock" INT,
+  "Id"  SERIAL PRIMARY KEY 
+);
+-- CREATE TABLE
+--
+--
+CREATE TABLE "Orders"(
+  "OrderNumber"  TEXT NOT NULL,
+  "DatePlaced"  TIMESTAMP NOT NULL,
+  "Email" TEXT, 
+  "Id"  SERIAL PRIMARY KEY 
+);
+-- CREATE TABLE
+--
+--
+-- JOIN table for products and orders 
+CREATE TABLE "ProductOrders"(
+  "OrderId"  INTEGER REFERENCES "Orders"("Id"),
+  "ProductId" INTEGER REFERENCES "Products"("Id"),
+  "Id"  SERIAL PRIMARY KEY, 
+  "OrderQuantity" INT NOT NULL 
+);
+-- CREATE TABLE
+--
+--
+INSERT INTO "Departments" ("DepartmentName", "Building") VALUES ('Development', 'Main');
+INSERT INTO "Departments" ("DepartmentName", "Building") VALUES ('Marketing', 'North');
+-- INSERT 0 1
+-- INSERT 0 1
+--
+--
+INSERT INTO "Employees" ("FullName",	"Salary",	"JobPosition",	"PhoneExtension",	"IsPartTime",	"DepartmentId") VALUES ('Tim Smith',	40000,	'Programmer',	123,	FALSE,	1);
+INSERT INTO "Employees" ("FullName",	"Salary",	"JobPosition",	"PhoneExtension",	"IsPartTime",	"DepartmentId") VALUES ('Barbara Ramsey',	80000,	'Manager',	234,	FALSE,	1);
+INSERT INTO "Employees" ("FullName",	"Salary",	"JobPosition",	"PhoneExtension",	"IsPartTime",	"DepartmentId") VALUES ('Tom Jones',	32000,	'Admin',	456,	TRUE,	2);
+-- INSERT 0 1
+-- INSERT 0 1
+-- INSERT 0 1
+--
+--
+INSERT INTO "Products" ("Price",	"Name",	"Description",	"QuantityInStock") VALUES (12.45,	'Widget',	'The Original Widget',	100);
+INSERT INTO "Products" ("Price",	"Name",	"Description",	"QuantityInStock") VALUES (99.99,	'Flowbee',	'Perfect for haircuts',	3);
+-- INSERT 0 1
+-- INSERT 0 1
+--
+--
+INSERT INTO "Orders" ("OrderNumber",	"DatePlaced",	"Email") VALUES ('X529',	'2020-01-01 16:55:00',	'person@example.com');
+-- INSERT 0 1
+--
+--
+INSERT INTO "ProductOrders" ("OrderId",	"ProductId",	"OrderQuantity") VALUES (1,	1, 3);
+INSERT INTO "ProductOrders" ("OrderId",	"ProductId",	"OrderQuantity") VALUES (1,	2, 2);
+-- INSERT 0 1
+-- INSERT 0 1
+--
+--
+SELECT "Departments"."Building", "Departments"."DepartmentName", "Employees"."FullName"
+FROM "Employees"
+JOIN "Departments" ON "Employees"."DepartmentId" = "Departments"."Id"
+WHERE "Departments"."Building" = 'East';
+-- +------------+------------------+------------+
+-- | Building   | DepartmentName   | FullName   |
+-- |------------+------------------+------------|
+-- +------------+------------------+------------+
+-- SELECT 0
+--
+--
+SELECT "Departments"."Building", "Departments"."DepartmentName", "Employees"."FullName"
+FROM "Employees"
+JOIN "Departments" ON "Employees"."DepartmentId" = "Departments"."Id"
+WHERE "Departments"."Building" = 'North Side';
+-- +------------+------------------+------------+
+-- | Building   | DepartmentName   | FullName   |
+-- |------------+------------------+------------|
+-- +------------+------------------+------------+
+-- SELECT 0
+--
+-- 
+SELECT "Departments"."Building", "Departments"."DepartmentName", "Employees"."FullName"
+FROM "Employees"
+JOIN "Departments" ON "Employees"."DepartmentId" = "Departments"."Id"
+WHERE "Departments"."Building" = 'Main';
+-- +------------+------------------+----------------+
+-- | Building   | DepartmentName   | FullName       |
+-- |------------+------------------+----------------|
+-- | Main       | Development      | Tim Smith      |
+-- | Main       | Development      | Barbara Ramsey |
+-- +------------+------------------+----------------+
+-- SELECT 2
+--
+--
+SELECT "Orders"."OrderNumber", "Products"."Name", "Products"."Id", "ProductOrders"."OrderQuantity"
+FROM "Orders"
+JOIN "ProductOrders" ON "Orders"."Id" = "ProductOrders"."OrderId"
+JOIN "Products" ON "ProductOrders"."ProductId" = "Products"."Id"
+WHERE "ProductOrders"."ProductId" = 2;
+-- +---------------+---------+------+-----------------+
+-- | OrderNumber   | Name    | Id   | OrderQuantity   |
+-- |---------------+---------+------+-----------------|
+-- | X529          | Flowbee | 2    | 2               |
+-- +---------------+---------+------+-----------------+
+-- SELECT 1
+--
+--
+SELECT "Products"."Name", "ProductOrders"."OrderQuantity"
+FROM "Orders"
+JOIN "ProductOrders" ON "Orders"."Id" = "ProductOrders"."OrderId"
+JOIN "Products" ON "ProductOrders"."ProductId" = "Products"."Id"
+WHERE ("Orders"."OrderNumber" = 'X529' AND "Products"."Name" = 'Flowbee');
+-- --+---------+-----------------+
+-- | Name    | OrderQuantity   |
+-- |---------+-----------------|
+-- | Flowbee | 2               |
+-- +---------+-----------------+
+-- SELECT 1
+--
+--
+
